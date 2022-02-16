a2730b6666545854d978b4649aa7a2271702bc1f
Nataliy Husak <nataliy.husak@gmail.com>
2022-02-16 13:00:42 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const item = document.querySelector(".item");
+const placeholders = document.querySelectorAll(".placeholder");
+
+item.addEventListener("dragstart", dragstart);
+item.addEventListener("dragend", dragend);
+
+for (const placeholder of placeholders) {
+  // console.log("placeholder");
+  placeholder.addEventListener("dragover", dragover);
+  placeholder.addEventListener("dragenter", dragenter);
+  placeholder.addEventListener("dragleave", dragleave);
+  placeholder.addEventListener("drop", dragdrop);
+}
+
+function dragstart(event) {
+  // console.log("drag start");
+  event.target.classList.add("hold");
+  setTimeout(() => event.target.classList.add("hide"), 0);
+}
+function dragend(event) {
+  // console.log("drag end");
+  // event.target.classList.remove("hold", "hide");
+  event.target.className = "item";
+}
+
+function dragover(event) {
+  // console.log("drag over");
+  event.preventDefault();
+}
+function dragenter(event) {
+  // console.log("drag enter");
+  event.target.classList.add("hovered");
+}
+function dragleave(event) {
+  // console.log("drag leave");
+  event.target.classList.remove("hovered");
+}
+function dragdrop(event) {
+  // console.log("drag drop");
+  event.target.classList.remove("hovered");
+  event.target.append(item);
+}
